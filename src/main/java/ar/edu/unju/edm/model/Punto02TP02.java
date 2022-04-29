3f909467ba237dd2b9a004d34227c42d578abd94
gustso <gustso@fi.unju.edu.ar>
2022-04-29 16:29:50 -0300

    Gustavo Sosa: Resolución punto 02

---
+++ src/main/java/ar/edu/unju/edm/model/Punto02TP02.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.edm.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Punto02TP02 {
+ public Punto02TP02() {
+	// TODO Auto-generated constructor stub
+}
+}
