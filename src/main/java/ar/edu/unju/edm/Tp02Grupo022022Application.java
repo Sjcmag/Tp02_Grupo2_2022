c7348a16a9e4b03600d10a005b984f1a71009005
javier <javier@10.0.3.170>
2022-05-05 14:20:47 -0300

    Punto 5 ,6 _ Colque Santiago

--- src/main/java/ar/edu/unju/edm/Tp02Grupo022022Application.java
+++ src/main/java/ar/edu/unju/edm/Tp02Grupo022022Application.java
@@ -3,11 +3,18 @@ package ar.edu.unju.edm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ar.edu.unju.edm.model.Punto5;
+import ar.edu.unju.edm.model.Punto6;
+
 @SpringBootApplication
 public class Tp02Grupo022022Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Tp02Grupo022022Application.class, args);
+		Punto5 conver = new Punto5("Hola Mundo!");		
+		System.out.println("es:"+conver.Cadena());
+		
 	}
 
 }
